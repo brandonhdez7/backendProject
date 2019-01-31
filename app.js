79613b565d484a2c42b0a76c5d416f78efe17b37
brandon <brandonhdez7@gmail.com>
2019-01-31 09:28:56 -0500

    some changes

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -38,6 +37,45 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+
+
+
+var bodyParser = require('body-parser');
+// var express = require('express');
+var plaid = require('plaid');
+
+// We store the access_token in memory - in production, store it in 
+// a secure
+// persistent data store
+var ACCESS_TOKEN = null;
+var PUBLIC_TOKEN = null;
+
+var client = new plaid.Client(
+  '5c4f83d1ca63910011f18edb',
+  '575f24b01bf3d0723f783f3d7be994',
+  '643da13c8f8bdb1c24f57ac1d23702',
+  plaid.environments.sandbox
+);
+
+// Accept the public_token sent from Link
+// var app = express();
+app.post('/get_access_token', function(request, response, next) {
+  PUBLIC_TOKEN = request.body.public_token;
+  client.exchangePublicToken(PUBLIC_TOKEN, function(error, 
+tokenResponse) {
+    if (error != null) {
+      console.log('Could not exchange public_token!' + '\n' + 
+error);
+      return response.json({error: msg});
+    }
+    ACCESS_TOKEN = tokenResponse.access_token;
+    ITEM_ID = tokenResponse.item_id;
+    console.log('Access Token: ' + ACCESS_TOKEN);
+    console.log('Item ID: ' + ITEM_ID);
+    response.json({'error': false});
+  });
 });
 
 module.exports = app;
