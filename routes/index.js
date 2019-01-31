1b4d5eab705e5480d607940d650c6063470b4174
Brian Lafuente <loulafuente@BrianLafuentes-Macbook-Pro.local>
2019-01-31 16:32:38 -0500

    jan 31

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const bcrypt = require('bcrypt-nodejs');
 const expressSession = require('express-session');
 
+
 const mysql = require('mysql');
 const config = require('../config');
 const connection = mysql.createConnection(config.db);
@@ -22,43 +23,6 @@ router.get('/login', function(req, res) {
 router.post('/loginProcess',(req,res)=>{
   // // login process
   res.json(req.body);
-  // const email = req.body.userEmail;
-  // // this is the English version of the password the user submitted
-  // const password = req.body.password;
-  // // we now need to get the hashed version fro mthe DB, and compare!
-  // const checkPasswordQuery = `SELECT * FROM users WHERE userEmail =?`;
-  // connection.query(checkPasswordQuery,[userEmail],(error, results)=>{
-  //     if(error){throw error;}
-  //     // possibilities:
-  //     // 1. No match, i.e. the user isn't not in the database.
-  //     if(results.length == 0 ){
-  //         // we don't care what password they gave us, send them back to /login
-  //         res.render('login?msg=noUser');
-  //     }else{
-  //         //user exists...
-  //         // 2. We found the user, but the password doesn't match
-  //         const passwordsMatch = bcrypt.compareSync(password,results[0].hash);
-  //         if(!passwordsMatch){
-  //             // goodbye
-  //             res.render('login?msg=badPass');
-  //         }else{
-  //             // 3. We found the user and the password matches
-  //             //these are the droids we're looking for!!
-  //             //-NOTE: every single http request (route) is 
-  //             // a completely new request
-  //             // Cookies: Stores data in the browser, with a key on the server 
-  //             // every single page request the entire cookie is sent to the server 
-  //             // Sessions: Stores data on the server, with a key(cookie) on the browser
-  //             req.session.name = results[0].userName;
-  //             req.session.email = results[0].userEmail;
-  //             // req.session.id = results[0].id;
-  //             req.session.uid = results[0].id;
-  //             req.session.loggedIn = true;
-  //             res.render('dashboard?msg=loginSuccess');
-  //             // response is set, HTTP disconnects, we are done
-  //         }        
-  //     }
-  // })
 })
 
 
