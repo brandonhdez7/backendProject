1e0614ee1a261796c5b377c97d3514fbaf490d62
brandon <brandonhdez7@gmail.com>
2019-02-01 11:41:40 -0500

    budget fix

--- public/budget.js
+++ public/budget.js
@@ -12,7 +12,7 @@ class UI {
       this.expenseInput = document.getElementById("expense-input");
       this.amountInput = document.getElementById("amount-input");
       this.expenseList = document.getElementById("expense-list");
-      this.itemList = [];
+      this.itemList = [];                 
       this.itemID = 0;
     }
     submitBudgetForm(){
@@ -48,7 +48,7 @@ class UI {
         this.balance.classList.remove('showRed', 'showBlack');
         this.balance.classList.add('showGreen')
       }
-      else if(total > 0){
+      else if(total = 0){
         this.balance.classList.remove('showRed', 'showGreen');
         this.balance.classList.add('showBlack')
       }
@@ -178,48 +178,48 @@ eventListenters();
   
 
 
-// var ctx = document.getElementById("myChart3").getContext('2d');
+var ctx = document.getElementById("myChart3").getContext('2d');
 
-// var myChart = new Chart(ctx, {
-//     type: 'doughnut',
-//     data: {
-//         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255,99,132,1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         legend:{
-//             position: 'right',
-//             scales: {
-//                 yAxes: [{
-//                     ticks: {
-//                         beginAtZero:true
-//                     }
-//                 }]
-//             }
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        legend:{
+            position: 'right',
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
         
-//         }   
-//     }
-// });
+        }   
+    }
+});
 
 
 
