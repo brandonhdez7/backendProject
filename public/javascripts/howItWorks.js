13f7517e0065e20810c87ac3ad88fc781fedd984
Ron Fletcher <ronfletcher1@me.com>
2019-02-01 15:01:36 -0500

    backend project

---
+++ public/javascripts/howItWorks.js
@@ -0,0 +1,59 @@
+// Welcome to the Budget Hobo!!
+// The Budget Hobo allows the user to set up a budget, 
+// download your bank transactions, and view your budget performance.
+// ***How It Works***
+// - Register/Login
+// - Set Up Budget
+// - Connect to Bank
+//     - download transactions
+//     - 
+// - Graphs & Reports
+//      -spending % of actual; % of budget
+
+// Registration 
+// 1. Click on "Sign Up Now" and the registration Sign Up 
+// panel will be displayed. 
+// 2. Review the Privacy Policy and Terms and Conditions
+// 2. Enter your email address and create a password
+// 3. Click on Sign Up and you're all set up
+
+// Log In 
+// 1. Click on "Login" and the Login panel will be displayed. 
+// 2. Enter your email address and password
+// 3. Click on Enter and your Dasboard will be displayed
+
+
+// Monthly Budget Set Up
+// Income Categories - income categories are for any income
+// that you estimate receiving for the month
+// - Salary - enter your moonthy salary (check amount) 
+// - Other Income - enter any other income amunts
+//   you anticipate receiving for the month
+
+// Expense Categories
+//   housing - rent or mortgage amount
+//   homeInsurance - renters or homeowners insurance amount
+//   homeGas - gas bill for apartment or home
+//   homeElectric - electric bill for apartment or home
+//   homeEntertainment - cable tv or dish bill
+//   autoLoan - car loan payment if any
+//   autoRepair - auto repairs
+//   autoGas - monthly gas allowance
+//   phoneBill - phone bill for home phone and any mobile phones
+//   groceries - amount spent directly on groceries
+//   homeSupplies - bathroom supplies, kitchen supplies, etc...
+//   eatingOut - amounts spent on take out and dining out
+//   clothing - amount spent on clothing
+//   vacation - amount estimated to save for that well deserved 
+//              vacation
+//   miscExp - any expense that is not categorized
+
+// Actual Expenditures
+//   Connect to your bank and download transactions
+//   Review and update expense categories
+//   Upload/Import into budget
+
+// Budget Review
+//   Weekly Snapshot
+//   Monthly Snapshot
+//   Annual Review
