8137354de5d796eb4d21f7b804f8f330f9295e0d
Brian Lafuente <loulafuente@BrianLafuentes-Macbook-Pro.local>
2019-02-04 10:34:53 -0500

    feb 4

--- routes/index.js
+++ routes/index.js
@@ -180,14 +180,6 @@ router.post('/loginProcess',(req,res)=>{
               } 
             })
   })
-                  req.session.profileImage = results[0].imageProfile
-                  res.redirect('/dashboard?msg=loginSuccess');
-
-                  // response is set, HTTP disconnects, we are done
-              }        
-          }
-      })
-})
 
 
 
