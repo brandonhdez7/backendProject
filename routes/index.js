beb781acb652244a7681beeaea4b4cbc0bc7b486
Brian Lafuente <loulafuente@BrianLafuentes-Macbook-Pro.local>
2019-02-04 13:43:37 -0500

    feb 4 after lunch

--- routes/index.js
+++ routes/index.js
@@ -203,10 +203,10 @@ router.get('/budget', function(req, res) {
 });
 
 router.get('/bank', function(req, res) {
-  console.log('in bank route')
   const selectQuery = `SELECT access FROM users WHERE userName LIKE '${res.locals.name}';`;
   connection.query(selectQuery,(error, data)=>{
-    console.log(data[0].access)
+    console.log(data)
+    // console.log(data[0].access)
     if (data[0].access != null){
       res.render('bank',{
         if(error){throw error;}
